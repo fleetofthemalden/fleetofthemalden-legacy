167766e4be0bff90c7fbc139b4d85dcee936bb79
Dixon Minnick <dewey.moor206@gmail.com>
2013-11-13 20:32:44 -0500

    fixing typo

--- cal/today.js
+++ cal/today.js
@@ -1,23 +1,32 @@
-
+var TEST;
 
 function getWorkouts(cal){
 	var wkts = new Array();
 	var now = new Date()
-	var qry = "http://fleetofthemalden.herokuapp.com/cal.json?month=" + (now.getMonth() + 1) + "&day" + (now.getDay() + 1) + "&cal=" + cal;
+	var qry = "http://fleetofthemalden.herokuapp.com/cal.json?month=" + (now.getMonth() + 1) + "&day=" + now.getDate() + "&cal=" + cal;
+	//var qry = "http://fleetofthemalden.herokuapp.com/cal.json?month=" + (now.getMonth() + 1) + "&cal=" + cal;
 	//var qry = "http://fleetofthemalden.herokuapp.com/cal.json?cal=" + cal;
+	//TEST = qry;
 	$.get(qry, function (workouts){
+		TEST = workouts;
 		if(workouts.length == 0){
 			var text = "<p>None</p>";
 			$("#main").append(text);
 		}
 		if(workouts.length == 1){
-			var text = workouts[0].a;
-			document.location = text.split("'")[1];
+			var text = getWorkoutText(workouts[0], 0);
+			var temp = text.split("'")[1];
+			if(temp == "clickable"){
+				$("#main").append(text);
+			}
+			else{
+				document.location = temp;
+			}
 		}
 		else{
 			var text = "<ul>";
 			for(var i=0; i<workouts.length; i++){
-				text += "<li>" + workouts[i].a + "</li>";
+				text += "<li>" + getWorkoutText(workouts[i], i) + "</li>";
 			}
 			text += "</ul>";
 			$("#main").append(text);
@@ -25,4 +34,19 @@ function getWorkouts(cal){
 	});
 	
 }
-	
+
+function getWorkoutText(wkt, i){
+	var text = wkt.a;
+	text = text.split("'>");
+	if(text[0] == "<a href='cust"){
+		text = text[1].split('</a>')[0];
+		var funBody = '$("#d' + i + '").html("&nbsp;&nbsp;&nbsp;&nbsp;' + wkt.descrip + '");';
+		var script = '<script>$("#a' + i + '").click(function(){' + funBody + '});</script>';
+		text = '<a class="clickable" id="a' + i + '">' + text + '</a>';
+		text += '<div id="d' + i + '">' + script + '</div>';
+		return text;
+	}
+	else{
+		return wkt.a;
+	}
+}
