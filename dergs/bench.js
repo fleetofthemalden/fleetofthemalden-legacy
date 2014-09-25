35127ed7e5bcc4cb3ad4cdd5712ab2b4fce48d35
Dixon Minnick <dewey.moor206@gmail.com>
2014-09-25 14:22:50 -0400

    fixing bench

--- dergs/bench.js
+++ dergs/bench.js
@@ -60,6 +60,15 @@ function parse() {
 		else{
 			wkt = workout[0];
 			wkt.Interval_work = new Array();
+			if(wkt.Interval == null){
+				wkt.Interval = wkt['Interval[]']
+			}
+			if(wkt.Rate == null){
+				wkt.Rate = wkt['Rate[]']
+			}
+			if(wkt.Percent2k == null){
+				wkt.Percent2k = wkt['Percent2k[]']
+			}
 			if(wkt.type == "time"){
 				wkt.atype = "Meters"
 				work_it = distancer;
